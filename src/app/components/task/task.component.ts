00fc914bc6909cd82134a23ece3b052a7390964e
chloetohce <chloetohce@gmail.com>
2025-03-01 19:42:51 +0800

    added: localstorage

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -16,27 +16,30 @@ export class TaskComponent implements OnChanges, OnInit {
   visibleButtonText: string = this.isCompletedVisible ? "Hide completed" : "Show completed"
 
   ngOnInit(): void {
-      this.tasks = []
+    this.tasks = JSON.parse(localStorage.getItem("tasks") ?? "[]")
   }
   
   ngOnChanges(changes: SimpleChanges): void {
       if (changes['newTask'] && !changes['newTask'].isFirstChange()) {
         this.tasks.push(changes['newTask'].currentValue)
+        localStorage.setItem("tasks", JSON.stringify(this.tasks))
       }
-  }
-
-  protected processComplete(event: any, idx: number) {
+      
+    }
+    
+    protected processComplete(event: any, idx: number) {
       const curr: Task = this.tasks[idx];
       curr.completion = event.target.checked;
-    console.log(this.tasks)
-  }
-
-  protected toggleCompletedVisibility() {
-    this.isCompletedVisible = !this.isCompletedVisible;
-    this.visibleButtonText = this.isCompletedVisible ? "Hide completed" : "Show completed"
-  }
-
-  protected deleteTask(idx: number) {
-    this.tasks = this.tasks.slice(idx, idx)
+      localStorage.setItem("tasks", JSON.stringify(this.tasks))
+    }
+    
+    protected toggleCompletedVisibility() {
+      this.isCompletedVisible = !this.isCompletedVisible;
+      this.visibleButtonText = this.isCompletedVisible ? "Hide completed" : "Show completed"
+    }
+    
+    protected deleteTask(idx: number) {
+      this.tasks.splice(idx, 1)
+      localStorage.setItem("tasks", JSON.stringify(this.tasks))
   }
 }
